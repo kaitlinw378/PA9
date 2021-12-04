28a7dfdc14650b4ccf92d2751f0f0df2ab98f3c4
kaitlinw378 <whitemkaitlin@gmail.com>
2021-12-03 21:25:49 -0800

    add

--- main.cpp
+++ main.cpp
@@ -7,12 +7,9 @@ using std::cout;
 using std::endl;
 
 int main() {
-<<<<<<< HEAD
 	cout << "test program" << endl;
 	cout << "hola" << endl;
-=======
 	cout << "test" << endl;
->>>>>>> 28afc14803d6460fb59a33c3f40cb075482b4806
 	cout << "hello world" << endl;
 	int x = 5;
 	return 0;
