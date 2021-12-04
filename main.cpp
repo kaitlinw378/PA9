420900aa2f967657d535dc88b907e1757897e43e
kaitlinw378 <whitemkaitlin@gmail.com>
2021-12-03 20:43:38 -0800

    test

--- main.cpp
+++ main.cpp
@@ -1,7 +1,14 @@
 //#include <SFML/Graphics.hpp>
 
+#include <string>
+#include <iostream>
+
+using std::cout;
+using std::endl;
+
 int main() {
 
+	cout << "hello" << endl;
 	int x = 5;
 	return 0;
 }
