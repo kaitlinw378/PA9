c7f35fad1ef82ac6c6610b7e25f2ac961ae3ff9b
kaitlinw378 <whitemkaitlin@gmail.com>
2021-12-06 14:16:00 -0800

    sfml libraries

---
+++ GamePiece.cpp
@@ -0,0 +1,16 @@
+#include "GamePiece.h"
+
+GamePiece GamePiece::createPiece() {
+	GamePiece piece;
+	piece.s1[0] = sf::Vector2i{ 1,0 };
+	piece.s1[1] = sf::Vector2i{ 1,1 };
+	piece.s1[2] = sf::Vector2i{ 1,2 };
+	piece.s1[3] = sf::Vector2i{ 1,3 };
+	return piece;
+}
+
+void GamePiece::displayPiece() {
+	const GamePiece nP = createPiece();
+	sf::RenderWindow window(sf::VideoMode(500, 500), "SFML works!");
+	//window.draw(nP);
+}
---
+++ GamePiece.h
@@ -0,0 +1,16 @@
+#pragma once
+
+#include <SFML/Graphics.hpp>
+
+using sf::Vector2i;
+
+class GamePiece {
+public:
+	GamePiece createPiece();
+	void displayPiece();
+
+private:
+
+	Vector2i s1[4];
+	
+};
