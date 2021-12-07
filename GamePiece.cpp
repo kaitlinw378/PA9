479165fed6cb7effe76115e1c3668b9d29ac3dc8
kaitlinw378 <whitemkaitlin@gmail.com>
2021-12-06 20:10:22 -0800

    gamepiece update

--- GamePiece.cpp
+++ GamePiece.cpp
@@ -7,11 +7,10 @@ GamePiece GamePiece::createPiece() {
 	piece.s1[2] = sf::Vector2i{ 1,2 };
 	piece.s1[3] = sf::Vector2i{ 1,3 };
 	return piece;
-	
 }
 
 void GamePiece::displayPiece() {
-	const GamePiece nP = createPiece();
-	sf::RenderWindow window(sf::VideoMode(500, 500), "SFML works!");
+	//const GamePiece nP = createPiece();
+	//sf::RenderWindow window(sf::VideoMode(500, 500), "SFML works!");
 	//window.draw(nP);
 }
