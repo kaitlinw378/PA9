e57b3d54e5703dc61adb62c55fcd8f865ed6ef4b
spencervs <s.vanderschalie@wsu.edu>
2021-12-10 17:19:23 -0800

    s

---
+++ test.cpp
@@ -0,0 +1,36 @@
+#include "test.h"
+
+bool test::testApplicationLoadingPercentage()
+{
+	bool result;
+
+	return result;
+}
+
+bool test::testApplicationDisplayMenu()
+{
+	bool result;
+
+	return result;
+}
+
+bool test::testTetrisDisplayMenu()
+{
+	bool result;
+
+	return result;
+}
+
+bool test::testScoreFunction()
+{
+	bool result;
+
+	return result;
+}
+
+bool test::testsomethin()
+{
+	bool result;
+
+	return result;
+}
