311df841577e74ae0a6207bd4f9cf73401d3b9af
spencervs <s.vanderschalie@wsu.edu>
2021-12-10 18:32:03 -0800

    s

--- test.cpp
+++ test.cpp
@@ -1,36 +1,37 @@
 #include "test.h"
 
-bool test::testApplicationLoadingPercentage()
+bool test::testApplicationLoadingPercentage(Application run)
 {
-	bool result;
-
+	bool result = false;
+	//run.displayStartUpScreen();
+	
 	return result;
 }
 
 bool test::testApplicationDisplayMenu()
 {
-	bool result;
+	bool result = false;
 
 	return result;
 }
 
 bool test::testTetrisDisplayMenu()
 {
-	bool result;
+	bool result = false;
 
 	return result;
 }
 
 bool test::testScoreFunction()
 {
-	bool result;
+	bool result = false;
 
 	return result;
 }
 
 bool test::testsomethin()
 {
-	bool result;
+	bool result = false;
 
 	return result;
 }
---
+++ test.h
@@ -0,0 +1,23 @@
+#pragma once
+
+#include "Piece.h"
+#include "Blocks.h"
+#include "Application.h"
+#include "Tetris.h"
+#include "Rectangle.h"
+
+
+class test
+{
+public:
+	test();
+	~test();
+
+	bool testApplicationLoadingPercentage(Application run);
+	bool testApplicationDisplayMenu();
+	bool testTetrisDisplayMenu();
+	bool testScoreFunction();
+	bool testsomethin();
+
+private:
+};
